ec295040cf6129e79693d0e2034e3c135dc82132
Mahesh Sathvika <79297927+sath-123@users.noreply.github.com>
2023-05-22 13:37:29 +0530

    pca results

---
+++ project/headers.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import auc
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+
